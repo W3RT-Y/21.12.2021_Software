626ae0cd0afe5fd4a70ce347b24c150a7451b3df
W3RT-Y <myyagcioglu@outlook.com>
2021-12-21 15:55:40 +0300

    1 version

---
+++ 20.12.2021 OOP.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace dogs
+{
+    public abstract class Animal
+    {
+       
+        public abstract string bark();
+    }
+
+    public class Dog : Animal
+    {
+        private string name;
+
+        public Dog() { }
+
+        public 
+    }
+    public class SheepDog : Dog
+    {
+        public override string bark()
+        {
+            public Dog(string nm) : base(nm)
+            {
+
+            }
+
+
+
+            Random random = new Random();
+            int min = 1;
+            int max = 10;
+            string barking = "";
+            int number = random.Next(min, max);
+            for(int i = 0; i < number; i++)
+            {
+                barking += "now-wow";
+            }
+            return (barking);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+C:\Windows\WinSxS\x86_netfx4 - csc_exe_b03f5f7f11
+
+C:\Windows\WinSxS\wow64_microsoft - windows - scripting_31bf3856ad364e35_10.0.19041.1237_none_c77fb947e9eed73b\r
